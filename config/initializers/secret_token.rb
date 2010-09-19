764328447e8e3e52bdd69b39aa53dea13ecfedeb
Will Leinweber <will@bitfission.com>
2010-09-19 13:31:42 -0500

    rails new . -OJT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3CouchrestModelExample::Application.config.secret_token = '7dcf5a09c0862c4a776d924c3b8117e69fc961130aac567c9106e0c7d08030208f0571dcb3313c5404e54d45cac5a7e6450a7270cac88ffb6f7b5d907ea034a5'
